29b31207a2460a4444187494c2280518a167c1ee
Roberto Fernández <61292228+robertofdzb10@users.noreply.github.com>
2024-10-14 10:00:42 +0200

    Delete CampingRioNela-master directory

--- CampingRioNela-master/static/js/videoManager.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var video = document.getElementById('hero-video');
-    var heroSection = document.querySelector('.hero');
-    
-    // Ajustar la velocidad del video al cargar
-    video.playbackRate = 0.8;
-});
